d11c1898a3fe2fed9d4ff54fb54df7d5c873ee96
ilya <ilyapn@outlook.com>
2020-04-13 09:31:17 +0300

    add authorization

--- src/main/java/wst/config/SoapConfig.java
+++ src/main/java/wst/config/SoapConfig.java
@@ -1,8 +1,15 @@
 package wst.config;
 
+import org.apache.cxf.configuration.security.AuthorizationPolicy;
+import org.apache.cxf.endpoint.Client;
+import org.apache.cxf.frontend.ClientProxy;
+import org.apache.cxf.interceptor.security.SimpleAuthorizingInterceptor;
 import org.apache.cxf.jaxws.JaxWsProxyFactoryBean;
+import org.apache.cxf.phase.Phase;
+import org.apache.cxf.transport.http.HTTPConduit;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.support.BasicAuthenticationInterceptor;
 import wst.generated.DB;
 
 @Configuration
@@ -14,5 +21,29 @@ public class SoapConfig {
         jaxWsProxyFactoryBean.setServiceClass(DB.class);
         jaxWsProxyFactoryBean.setAddress("http://localhost:7070/services/db");
         return (DB) jaxWsProxyFactoryBean.create();
+    }
+
+    @Bean
+    public Client ticketAgentClientProxy() {
+        return ClientProxy.getClient(db());
+    }
+
+    @Bean
+    public HTTPConduit ticketAgentConduit() {
+        HTTPConduit httpConduit = (HTTPConduit) ticketAgentClientProxy().getConduit();
+        httpConduit.setAuthorization(basicAuthorization());
+
+        return httpConduit;
+    }
+
+    @Bean
+    public AuthorizationPolicy basicAuthorization() {
+        AuthorizationPolicy authorizationPolicy =
+                new AuthorizationPolicy();
+        authorizationPolicy.setUserName("1");
+        authorizationPolicy.setPassword("1");
+        authorizationPolicy.setAuthorizationType("Basic");
+
+        return authorizationPolicy;
     }
 }
